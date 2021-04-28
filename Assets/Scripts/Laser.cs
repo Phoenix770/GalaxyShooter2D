bf3b8d55d27aab7dce71fa751720707d0fffbe18
Phoenix770 <matej.marek94@gmail.com>
2021-04-28 20:43:27 +0200

    Added laser prefab & behavior

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] float _speed = 9f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y > 7f)
+            Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _playerSpeed = 7f;
+    [SerializeField] GameObject _laserPrefab;
+    [SerializeField] float _fireRate = 0.15f;
+    Transform _laserContainer;
+    float _canFire = -1f;
 
     void Start()
     {
         transform.position = new Vector3(0, -4f, 0);
+        _laserContainer = GameObject.Find("LaserContainer").GetComponent<Transform>();
+        if (_laserContainer == null)
+            Debug.LogError("Laser Container is NULL!");
     }
 
     void Update()
@@ -20,5 +27,17 @@ public class Player : MonoBehaviour
 
         if (transform.position.x > 11f || transform.position.x < -11f)
             transform.position = new Vector3(-transform.position.x, transform.position.y, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        _canFire = Time.time + _fireRate;
+        GameObject laser = Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        laser.transform.SetParent(_laserContainer);
     }
 }
